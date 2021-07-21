63a744df38ae1f935c5b92b8336bb54b397dcc4a
ksjj4791 <ksjj4791@gmail.com>
2021-07-21 13:05:24 +0900

    작성자 : 김송주 내용 : 새 프로젝트 삽입

---
+++ 20210719_Profile/src/main/java/com/min/edu/HomeController.java
@@ -0,0 +1,39 @@
+package com.min.edu;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+@RequestMapping("/")
+public class HomeController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	@Value("${evn.text}")
+	String envtext;
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/home.do", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		logger.info("설정된 db.properties profile {}", envtext);
+		
+		model.addAttribute("envtext", envtext);
+		
+		return "home";
+	}
+	
+}
---
+++ 20210719_Profile/src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,13 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ page session="false" %>
+<html>
+<head>
+	<title>Home</title>
+</head>
+<body>
+<h1>
+	Hello world!  
+</h1>
+<h2>${envtext} : 전달값</h2>
+</body>
+</html>
